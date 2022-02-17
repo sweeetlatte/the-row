6e2a94eb7d0dd34d33474d9fa302e4603b0bc18c
sweetcapucino <thuylinh271055@gmail.com>
2022-02-17 15:23:25 +0700

    U: hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+    return (
+        <div className="App">
+            <div className="mx-auto">
+                <div className="w-screen h-screen banner-img grid content-center">
+                    <div
+                        className="2xl:text-9xl xl:text-8xl md:text-6xl sm:text-4xl font-bold"
+                        style={{ color: "#3C3B3B" }}
+                    >
+                        THE ROW
+                    </div>
+                </div>
+                <div className="row">
+                    Shop
+                    <div className="flex">
+                        <div className="basis-1/2">men</div>
+                        <div className="basis-1/2">women</div>
+                    </div>
+                </div>
+                <div className="row">menu</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
