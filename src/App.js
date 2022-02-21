e02027691bc84ce04675e818bb61eeede96c645b
sweetcapucino <thuylinh271055@gmail.com>
2022-02-21 14:53:08 +0700

    M: navbar items-center

--- src/App.js
+++ src/App.js
@@ -44,15 +44,15 @@ function App() {
                         </div>
                     </div>
                 </div>
-                <div className="w-screen bg-[#D4D0D0] h-32 grid content-center banner-img">
-                    <div className="flex">
-                        <div className="basis-9/12 flex text-4xl">
-                            <div className="basis-1/4">About us</div>
-                            <div className="basis-1/4">Contact us</div>
-                            <div className="basis-1/4">Customer care</div>
-                            <div className="basis-1/4">Instagram</div>
-                        </div>
-                        <div className="basis-3/12 text-5xl">THE ROW</div>
+                <div className="w-screen bg-[#D4D0D0] h-32 flex items-center banner-img">
+                    <div className="basis-9/12 flex items-center 2xl:text-4xl xl:text-3xl md:text-2xl sm:text-xl">
+                        <div className="basis-1/4">About us</div>
+                        <div className="basis-1/4">Contact us</div>
+                        <div className="basis-1/4">Customer care</div>
+                        <div className="basis-1/4">Instagram</div>
+                    </div>
+                    <div className="basis-3/12 2xl:text-5xl xl:text-4xl md:text-3xl sm:text-2xl">
+                        THE ROW
                     </div>
                 </div>
             </div>
