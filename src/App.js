5f8084b9bbd913af2505548f1ffa0de58c12b3ae
sweetcapucino <thuylinh271055@gmail.com>
2022-02-21 14:45:52 +0700

    U: navbar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
                         THE ROW
                     </div>
                 </div>
-                <div className="row mt-14">
+                <div className="row mt-14 mb-28">
                     <div
                         className="2xl:text-9xl xl:text-8xl md:text-6xl sm:text-4xl font-light mb-12"
                         style={{ color: "#4D4D4D" }}
@@ -44,7 +44,17 @@ function App() {
                         </div>
                     </div>
                 </div>
-                <div className="row">navbar</div>
+                <div className="w-screen bg-[#D4D0D0] h-32 grid content-center banner-img">
+                    <div className="flex">
+                        <div className="basis-9/12 flex text-4xl">
+                            <div className="basis-1/4">About us</div>
+                            <div className="basis-1/4">Contact us</div>
+                            <div className="basis-1/4">Customer care</div>
+                            <div className="basis-1/4">Instagram</div>
+                        </div>
+                        <div className="basis-3/12 text-5xl">THE ROW</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
